c5d7419a508c22ea592422137aca8e273e93ba5d
@hyh17808770899 <5347752+hyh17808770899@user.noreply.gitee.com>
2021-12-21 16:30:59 +0800

    集成MySQL8、MyBatis-Plus、Knife4j

---
+++ spring-boot/project-01/src/main/java/com/yuyun/service/CompanyService.java
@@ -0,0 +1,14 @@
+package com.yuyun.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yuyun.dto.CompanyDTO;
+
+/**
+ * 企业信息表
+ *
+ * @author
+ * @since 1.0.0 2021-12-17
+ */
+public interface CompanyService extends IService<CompanyDTO> {
+
+}
