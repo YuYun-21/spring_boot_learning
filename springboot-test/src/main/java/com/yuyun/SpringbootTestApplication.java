dedf60ce2e8908e68379a23e1fdb890b110c67c2
1873591403 <1873591403@qq.com>
2023-08-16 17:09:35 +0800

    新建测试模块，用来测试某些功能

--- springboot-test/src/main/java/com/yuyun/SpringbootTestApplication.java
+++ springboot-test/src/main/java/com/yuyun/SpringbootTestApplication.java
@@ -3,6 +3,9 @@ package com.yuyun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yuyun
+ */
 @SpringBootApplication
 public class SpringbootTestApplication {
 
