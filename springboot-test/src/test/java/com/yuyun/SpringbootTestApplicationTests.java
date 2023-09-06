87dd8f604794e592857641831dec3d6ab09ab936
1873591403 <1873591403@qq.com>
2023-09-06 15:39:00 +0800

    feat: 增加hutool工具，get请求测试

--- springboot-test/src/test/java/com/yuyun/SpringbootTestApplicationTests.java
+++ springboot-test/src/test/java/com/yuyun/SpringbootTestApplicationTests.java
@@ -1,9 +1,12 @@
 package com.yuyun;
 
+import cn.hutool.http.HttpUtil;
 import com.yuyun.task.DynamicCronTask;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class SpringbootTestApplicationTests {
@@ -15,6 +18,21 @@ class SpringbootTestApplicationTests {
     void contextLoads() {
         dynamicCronTask.list();
 
-        }
+    }
+
+    @Test
+    void restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "http://10.0.0.24:8083/wzkj-api/questionAnswer/page";
+        JSONObject forObject = restTemplate.getForObject(url, JSONObject.class);
+
+        System.out.println("forObject = " + forObject);
+
+        String response1 = HttpUtil.get(url);
+
+        System.out.println("response1 = " + response1);
+
+    }
 
 }
