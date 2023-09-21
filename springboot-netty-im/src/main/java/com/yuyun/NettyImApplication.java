6ba7f3bf32ee63bd3b7f803ec5c1716425af38f1
1873591403 <1873591403@qq.com>
2023-09-21 10:57:14 +0800

    feat：新建netty-im模块

---
+++ springboot-netty-im/src/main/java/com/yuyun/NettyImApplication.java
@@ -0,0 +1,13 @@
+package com.yuyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyImApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyImApplication.class, args);
+    }
+
+}
