21b48f1cd573e8cb5289d6bc2ac9c580a23f7d2e
1873591403 <1873591403@qq.com>
2023-10-10 15:22:56 +0800

    feat：开启ts严格模式，home界面优化

--- springboot-netty/netty-im-vue3/src/views/HomeView.vue
+++ springboot-netty/netty-im-vue3/src/views/HomeView.vue
@@ -1,12 +1,24 @@
 <template>
-  <RouterLink to="/about">About</RouterLink>
-  <RouterLink to="/nettyIm">NettyIm</RouterLink>
-  <RouterLink to="/websocket">Websocket</RouterLink>
+  <el-space :size="size" :spacer="spacer">
+      <el-button type="primary" plain>
+        <RouterLink to="/about">About</RouterLink>
+      </el-button>
+      <el-button type="primary" plain>
+        <RouterLink to="/nettyIm">NettyIm</RouterLink>
+      </el-button>
+      <el-button type="primary" plain>
+        <RouterLink to="/websocket">Websocket</RouterLink>
+      </el-button>
+  </el-space>
 </template>
 
 <script lang="ts" setup>
-
 import { RouterLink } from "vue-router";
+import { h, ref } from 'vue'
+import { ElDivider } from 'element-plus'
+
+const size = ref(10)
+const spacer = h(ElDivider, { direction: 'vertical' })
 
 </script>
 
