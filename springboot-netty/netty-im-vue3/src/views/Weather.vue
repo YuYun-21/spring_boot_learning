fc6612b5c7eb8870bc09afb5d77be82e234f6bb0
1873591403 <1873591403@qq.com>
2023-11-08 14:57:32 +0800

    feat：更新netty-im-vue3页面：新增vue、html天气小组件

---
+++ springboot-netty/netty-im-vue3/src/views/Weather.vue
@@ -0,0 +1,61 @@
+<script src="http://widget.seniverse.com/widget/chameleon.js"></script>
+<script>
+export default {
+  name: "Weather",
+  created() {
+    this.initWeatherWidget()
+  },
+  methods: {
+    initWeatherWidget() {
+      (function (T, h, i, n, k, P, a, g, e) {
+        g = function () {
+          P = h.createElement(i);
+          a = h.getElementsByTagName(i)[0];
+          P.src = k;
+          P.charset = "utf-8";
+          P.async = 1;
+          a.parentNode.insertBefore(P, a);
+        };
+        T["ThinkPageWeatherWidgetObject"] = n;
+        T[n] || (T[n] = function () {
+          (T[n].q = T[n].q || []).push(arguments);
+        });
+        T[n].l = +new Date();
+        if (T.attachEvent) {
+          T.attachEvent("onload", g);
+        } else {
+          T.addEventListener("load", g, false);
+        }
+      }(window, document, "script", "tpwidget", "//widget.seniverse.com/widget/chameleon.js"));
+
+      tpwidget("init", {
+        "flavor": "slim",
+        "location": "WK3N92NQV6RQ",
+        "geolocation": "disabled",
+        "language": "zh-chs",
+        "unit": "c",
+        "theme": "chameleon",
+        "container": "tp-weather-widget",
+        "bubble": "enabled",
+        "alarmType": "badge",
+        "uid": "UE8EB120D0",
+        "hash": "c74526577e358dfb1ed5eef1e0218c31"
+      });
+      tpwidget("show");
+    }
+
+  }
+}
+</script>
+
+<template>
+  <div class="headerinfo">
+    <span id="dateweek"></span>
+    <div id="tp-weather-widget">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
