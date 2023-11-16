d3f2cdc73d7229399edcfae0df7fdf63b90a8023
1873591403 <1873591403@qq.com>
2023-11-16 16:31:03 +0800

    feat: 新增hutool工具测试和正则表达式测试

---
+++ springboot-test/src/test/java/com/yuyun/RegularExpressionTest.java
@@ -0,0 +1,40 @@
+package com.yuyun;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 正则表达式
+ *
+ * @author hyh
+ * @since 2023-11-15
+ */
+public class RegularExpressionTest {
+
+    @Test
+    void test(){
+        String input = "帮扶工 作 。2023年7月27。  哈哈哈哈骗你的";
+
+        // 使用正则表达式替换文字后面的空格
+        String result = input.replaceAll("(?<=[\\u4e00-\\u9fa5])\\s+", "");
+
+        System.out.println("替换后的结果：" + result);
+
+        String result1 = input.replaceAll("\\s+(?=[\\u4e00-\\u9fa5])", "");
+
+        System.out.println("替换后的结果：" + result1);
+    }
+
+    @Test
+    void test1(){
+        String input = "1234567890";
+
+        // 使用正则表达式替换文字后面的空格
+        String result = input.replaceAll("(?<=[\\u4e00-\\u9fa5])\\s+", "");
+
+        System.out.println("替换后的结果：" + result);
+
+        String result1 = input.replaceAll("\\s+(?=[\\u4e00-\\u9fa5])", "");
+
+        System.out.println("替换后的结果：" + result1);
+    }
+}
